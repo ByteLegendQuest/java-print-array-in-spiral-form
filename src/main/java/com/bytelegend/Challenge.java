3ec489a7890f831007a6861028bd2146e8ef29da
s-xiaoxiao <460046653@qq.com>
2022-04-28 15:07:56 +0000

    Update at 2022-04-28T15:07:56.120115Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,14 +1,16 @@
 package com.bytelegend;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(
                 Arrays.toString(
                         spiralOrder(
-                                new int[][] {
-                                    {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
+                                new int[][]{
+                                        {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
                                 })));
     }
 
@@ -22,6 +24,51 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
+        List<Integer> arr = new ArrayList<>();
+
+        int left = 0, right = matrix[0].length - 1;
+        int top = 0, down = matrix.length - 1;
+
+        while (true) {
+            for (int i = left; i <= right; ++i) {
+                arr.add(matrix[top][i]);
+            }
+
+            top++;
+            if (top > down) {
+                break;
+            }
+
+            for (int i = top; i <= down; ++i) {
+                arr.add(matrix[i][right]);
+            }
+
+            right--;
+            if (left > right) {
+                break;
+            }
+            ;
+
+            for (int i = right; i >= left; --i) {
+                arr.add(matrix[down][i]);
+            }
+
+            down--;
+            if (top > down) {
+                break;
+            }
+
+            for (int i = down; i >= top; --i) {
+                arr.add(matrix[i][left]);
+            }
+
+            left++;
+            if (left > right) {
+                break;
+            }
+
+        }
+
+        return arr.stream().mapToInt(Integer::intValue).toArray();
     }
 }
