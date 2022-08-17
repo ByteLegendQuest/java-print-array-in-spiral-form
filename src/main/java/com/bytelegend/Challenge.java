2a60cf56d50676c73e023554aa8d46c2a487424b
Anlyui <1391469813@qq.com>
2022-08-17 14:35:59 +0000

    Update at 2022-08-17T14:35:59.547205Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,6 @@
 package com.bytelegend;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Challenge {
@@ -22,6 +23,50 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
+        int[] ans = new int[matrix.length * matrix[0].length];
+        int iPlusCounter = 0;
+        int jPlusCounter = 0;
+        int iMinuxCounter = 0;
+        int jMinuxCounter = 0;
+        int index = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while (index < ans.length) {
+            while (j + jPlusCounter < matrix[i].length && index < ans.length) {
+                ans[index] = matrix[i][j];
+                index++;
+                j++;
+            }
+            j--;
+            i++;
+            jPlusCounter++;
+            while (i + iPlusCounter < matrix.length && index < ans.length) {
+                ans[index] = matrix[i][j];
+                index++;
+                i++;
+            }
+            i--;
+            j--;
+            iPlusCounter++;
+            while (j >= jMinuxCounter && index < ans.length) {
+                ans[index] = matrix[i][j];
+                index++;
+                j--;
+            }
+            j++;
+            i--;
+            jMinuxCounter++;
+            while (i > iMinuxCounter && index < ans.length) {
+                ans[index] = matrix[i][j];
+                index++;
+                i--;
+            }
+            i++;
+            j++;
+            iMinuxCounter++;
+        }
+        return ans;
     }
 }
