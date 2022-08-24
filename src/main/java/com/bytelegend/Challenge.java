5f031c4935274248e0712e70bc27b52ea6f8262a
agirnob <kakcayigitbaran@gmail.com>
2022-08-24 14:33:32 +0000

    Update at 2022-08-24T14:33:32.895119Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,7 @@
 package com.bytelegend;
-
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(
@@ -22,6 +22,34 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
-    }
+         List<Integer> ans = new ArrayList<Integer>();
+
+       
+
+        int m = matrix.length, n = matrix[0].length;
+        boolean[][] seen = new boolean[m][n];
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        int x = 0, y = 0, di = 0;
+
+        // Iterate from 0 to R * C - 1
+        for (int i = 0; i < m * n; i++) {
+            ans.add(matrix[x][y]);
+            seen[x][y] = true;
+            int cr = x + dr[di];
+            int cc = y + dc[di];
+
+            if (0 <= cr && cr < m && 0 <= cc && cc < n
+                    && !seen[cr][cc]) {
+                x = cr;
+                y = cc;
+            } else {
+                di = (di + 1) % 4;
+                x += dr[di];
+                y += dc[di];
+            }
+        }
+        int[] arr =
+                ans.stream().mapToInt(i -> i).toArray();
+        return arr;
 }
