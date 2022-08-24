2a03c6ccb63ce786b54f76d10ffa583bbb9cc6c7
agirnob <kakcayigitbaran@gmail.com>
2022-08-24 14:36:14 +0000

    Update at 2022-08-24T14:36:14.427754Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,14 +1,16 @@
 package com.bytelegend;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(
                 Arrays.toString(
                         spiralOrder(
-                                new int[][] {
-                                    {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
+                                new int[][]{
+                                        {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
                                 })));
     }
 
@@ -17,14 +19,17 @@ public class Challenge {
      *
      * <p>For example, given the following matrix:
      *
-     * <p>``` 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 ```
+     * <p>``` 1  2  3  4
+     * 5  6  7  8
+     * 9  10 11 12
+     * 13 14 15 16 ```
      *
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-         List<Integer> ans = new ArrayList<Integer>();
+        List<Integer> ans = new ArrayList<Integer>();
 
-       
+
 
         int m = matrix.length, n = matrix[0].length;
         boolean[][] seen = new boolean[m][n];
@@ -52,4 +57,7 @@ public class Challenge {
         int[] arr =
                 ans.stream().mapToInt(i -> i).toArray();
         return arr;
+
+
+    }
 }
