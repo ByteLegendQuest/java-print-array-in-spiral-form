bff94ce73ff7dd6d145e5bd3e20ef212ec9f3ed9
lhhhhhhhhhhhhhh <lihan881212@gmail.com>
2022-09-23 08:22:45 +0000

    Update at 2022-09-23T08:22:45.586688Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -22,6 +22,35 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
+        int index = 0;
+        int count = matrix.length * matrix[0].length;
+        int[] ret = new int[count];
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int top = 0;
+        int bottom = matrix.length - 1;
+        while (index < count) {
+            for (int i = left; i <= right; i++) {
+                ret[index] = matrix[top][i];
+                index++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                ret[index] = matrix[i][right];
+                index++;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                ret[index] = matrix[bottom][i];
+                index++;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                ret[index] = matrix[i][left];
+                index++;
+            }
+            left++;
+        }
+        return ret;
     }
 }
