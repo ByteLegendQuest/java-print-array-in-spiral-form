f6f1f7facd71634ee8572fc763efba74107e3dd9
Tomcat-Guo <zhiqiang.guo@shopee.com>
2022-11-28 06:33:23 +0000

    Update at 2022-11-28T06:33:23.149813Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -22,6 +22,34 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
+        int[] result = new int[matrix.length * matrix[0].length];
+        int j = 0;
+        int w = 0;
+        int x = matrix.length - 1;
+        int y = 0;
+        int z = matrix[0].length - 1;
+        while (w <= x && y <= z) {
+            for (int i = w; i <= z; i++) {
+                result[j++] = matrix[w][i];
+            }
+            for (int i = w + 1; i <= x; i++) {
+                result[j++] = matrix[i][z];
+            }
+            if (w + 1 <= x) {
+                for (int i = z - 1; i >= y; i--) {
+                    result[j++] = matrix[x][i];
+                }
+            }
+            if (y + 1 <= z) {
+                for (int i = x - 1; i > w; i--) {
+                    result[j++] = matrix[i][y];
+                }
+            }
+            w++;
+            x--;
+            y++;
+            z--;
+        }
+        return result;
     }
 }
