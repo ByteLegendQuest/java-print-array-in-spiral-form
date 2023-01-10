0b17947a904c24a72d2d4744ddf7b62bae857fc3
YoungerKayn <79000036+YoungerKayn@users.noreply.github.com>
2023-01-10 08:04:45 +0000

    Update at 2023-01-10T08:04:45.914904Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,8 +7,8 @@ public class Challenge {
         System.out.println(
                 Arrays.toString(
                         spiralOrder(
-                                new int[][] {
-                                    {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
+                                new int[][]{
+                                        {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
                                 })));
     }
 
@@ -22,6 +22,35 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
+        int rowLength = matrix.length;
+        int colLength = matrix[0].length;
+        int[] result = new int[rowLength * colLength];
+        int rowCursor = 0, colCursor = 0, k = 0, l = 0, cursor = 0, round = 0;
+        while (cursor < rowLength * colLength) {
+            for (colCursor = round; colCursor <= colLength - (round + 1) && cursor < rowLength * colLength; colCursor++) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            colCursor--;
+            for (rowCursor = round + 1; rowCursor <= rowLength - (round + 1) && cursor < rowLength * colLength; rowCursor++) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            rowCursor--;
+            for (colCursor--; colCursor >= round && cursor < rowLength * colLength; colCursor--) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            colCursor++;
+            for (rowCursor--; rowCursor >= round + 1 && cursor < rowLength * colLength; rowCursor--) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            rowCursor++;
+            round++;
+            rowCursor = round;
+            colCursor = round;
+        }
+        return result;
     }
 }
