14907b20c5edac5188b003a42e474e5859212d45
TheTenthYama <1355794322@qq.com>
2023-01-26 10:54:12 +0000

    Update at 2023-01-26T10:54:12.892297Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -22,6 +22,34 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] result = new int[row * col];
+        int rowCursor = 0, colCursor = 0, cursor = 0, round = 0;
+        while (cursor < row * col) {
+            for (colCursor = round; colCursor <= col - (round + 1) && cursor < row * col; colCursor++) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            colCursor--;
+            for (rowCursor = round + 1; rowCursor <= row - (round + 1) && cursor < row * col; rowCursor++) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            rowCursor--;
+            for (colCursor--; colCursor >= round && cursor < row * col; colCursor--) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            colCursor++;
+            for (rowCursor--; rowCursor >= round + 1 && cursor < row * col; rowCursor--) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            round++;
+            rowCursor = round;
+            colCursor = round;
+        }
+        return result;
     }
 }
