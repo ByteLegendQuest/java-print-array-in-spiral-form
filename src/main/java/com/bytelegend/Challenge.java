5ed547b8f26b16db31502757d192edbaada11dc9
DasBrain <johannes13@johannes13.de>
2023-05-20 17:20:28 +0000

    Update at 2023-05-20T17:20:28.327059Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -22,7 +22,7 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return new int[0];
         }
         int m = matrix.length;
