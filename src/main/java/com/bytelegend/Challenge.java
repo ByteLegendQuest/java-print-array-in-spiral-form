f349ef8d6d491bd34592d756711196bfe7308ede
HumansDoNotWantImmortality <rosinski.krzysztof.01@gmail.com>
2023-06-05 16:23:23 +0000

    Update at 2023-06-05T16:23:23.969194Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -22,6 +22,47 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int top = 0;
+        int bottom = rows - 1;
+        int left = 0;
+        int right = cols - 1;
+
+        List<Integer> spiralOrder = new ArrayList<>();
+
+        while (top <= bottom && left <= right) {
+            for (int col = left; col <= right; col++) {
+                spiralOrder.add(matrix[top][col]);
+            }
+            top++;
+
+            for (int row = top; row <= bottom; row++) {
+                spiralOrder.add(matrix[row][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int col = right; col >= left; col--) {
+                    spiralOrder.add(matrix[bottom][col]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int row = bottom; row >= top; row--) {
+                    spiralOrder.add(matrix[row][left]);
+                }
+                left++;
+            }
+        }
+
+        int[] result = new int[spiralOrder.size()];
+        for (int i = 0; i < spiralOrder.size(); i++) {
+            result[i] = spiralOrder.get(i);
+        }
+
+        return result;
     }
 }
