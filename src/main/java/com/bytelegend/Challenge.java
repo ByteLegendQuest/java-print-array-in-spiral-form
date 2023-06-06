9ae3343f9b6c8f36ab45411aa102e101800141d3
andyliukun <744059787@qq.com>
2023-06-06 05:55:14 +0000

    Update at 2023-06-06T05:55:14.144123Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,8 +7,8 @@ public class Challenge {
         System.out.println(
                 Arrays.toString(
                         spiralOrder(
-                                new int[][] {
-                                    {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
+                                new int[][]{
+                                        {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
                                 })));
     }
 
@@ -22,6 +22,33 @@ public class Challenge {
      * <p>Return `[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]`.
      */
     public static int[] spiralOrder(int[][] matrix) {
-        return null;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] result = new int[row * col];
+        int rowCursor = 0, colCursor, cursor = 0, round = 0;
+        while (cursor < row * col) {
+            for (colCursor = round; colCursor <= col - (round + 1) && cursor < row * col; colCursor++) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            colCursor--;
+            for (rowCursor = round + 1; rowCursor <= row - (round + 1) && cursor < row * col; rowCursor++) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            rowCursor--;
+            for (colCursor--; colCursor >= round && cursor < row * col; colCursor--) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            colCursor++;
+            for (rowCursor--; rowCursor >= round + 1 && cursor < row * col; rowCursor--) {
+                result[cursor] = matrix[rowCursor][colCursor];
+                cursor++;
+            }
+            round++;
+            rowCursor = round;
+        }
+        return result;
     }
 }
